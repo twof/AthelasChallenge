1f7a331941e89f140dd5bcc13fd1326153547dae
twof <fabiobean2@gmail.com>
2017-06-07 18:32:34 -0700

    Added button component to trigger polling

--- Components/MainPage.js
+++ Components/MainPage.js
@@ -29,13 +29,57 @@ var styles = StyleSheet.create({
         alignSelf: 'stretch',
         justifyContent: 'center'
     },
+    buttonText: {
+        fontSize: 18,
+        color: 'white',
+        alignSelf: 'center'
+    },
 });
 
+// The following two fuctions are set up for later extension
+function urlForStartingJob(deviceID) {
+    return 'http://api.athelas.com/devices/' + deviceID + '/start';
+}
 
-class PollingPage extends Component {
+function urlForPollingJob(jobID) {
+    return 'http://api.athelas.com/jobs/' + jobID + '/poll';
+}
+
+class MainPage extends Component {
     constructor(props) {
         super(props);
         this.state = {isLoading: false};
+    }
+
+    _handleResponse(response) {
+        this.setState({isLoading: false});
+
+        if(response.application_response_code.substr(0, 1) == '1') {
+            this.props.navigator.push({
+                title: 'Results',
+                component: SearchResults,
+                passProps: {listings: response.listings}
+            });
+        }else{
+            this.setState({message: 'Location not recongnized; please try again'});
+        }
+    }
+
+    _executeQuery(query) {
+        this.setState({isLoading: true}, () => {
+            fetch(query)
+                .then(response => response.json())
+                .then(json => this._handleResponse(json.response))
+                .catch(error =>
+                    this.setState({
+                        isLoading: false,
+                        message: 'Something bad happened ' + error
+                }));
+        });
+    }
+
+    onGetDataButtonPressed() {
+
     }
 
     render() {
@@ -44,11 +88,14 @@ class PollingPage extends Component {
             (<View/>);
 
         return (
-            <TouchableHighlight underlayColor='#99d9f4' style={styles.locationButton}>
-                <Text style={styles.buttonText}>Location</Text>
-            </TouchableHighlight>
+            <View style={styles.container}>
+                <TouchableHighlight underlayColor='#99d9f4' style={styles.button} onPress={this.onGetDataButtonPressed.bind(this)}>
+                    <Text style={styles.buttonText}>Get Test Data</Text>
+                </TouchableHighlight>
+                {spinner}
+            </View>
         );
     }
 }
 
-module.exports = PollingPage;
+module.exports = MainPage;
